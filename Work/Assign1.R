b15bccaa22ed8ea7bbd7249d86881efd150ffce3
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-13 11:09:52 -0500

    x

--- Work/Assign1.R
+++ Work/Assign1.R
@@ -1,5 +1,6 @@
-#initializing packages
+#initializing packages                          ###################Look up violin plots later, boxplots with probability density
 library(tidyverse)
 library(ggplot2)
+#library(magritter)
 
 #library(help="datasets") #suggested to find datasets
