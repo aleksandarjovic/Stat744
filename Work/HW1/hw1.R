70dfa2bf3f5cc5790a1152300cec0ff1486d0bd7
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-15 17:27:16 -0500

    adding some datasets

--- Work/HW1/hw1.R
+++ Work/HW1/hw1.R
@@ -1,8 +1,8 @@
-### Homework 1. Tues Jan 16. Aleksandar Jovic
-
+rm(list = ls())### Homework 1. Tues Jan 16. Aleksandar Jovic###
+#library(help="datasets") #suggested to find datasets
 #######Look up violin plots later, boxplots with probability density
+
 library(tidyverse) #loads magritter, dplyr, readr, tidyr, purr, tibble, stringr, forcats
 library(ggplot2)
 
-#library(help="datasets") #suggested to find datasets
 read_csv("data/myfile.csv") #need a data folder first for datasets being used
