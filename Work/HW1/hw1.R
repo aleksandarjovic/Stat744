6c2f2e51898ce15aeece08278a1185c745ddd23c
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-15 02:38:03 -0500

    keeping main changes away for now.

--- Work/HW1/hw1.R
+++ Work/HW1/hw1.R
@@ -1,6 +1,8 @@
-#initializing packages                          ###################Look up violin plots later, boxplots with probability density
-library(tidyverse)
+### Homework 1. Tues Jan 16. Aleksandar Jovic
+
+#######Look up violin plots later, boxplots with probability density
+library(tidyverse) #loads magritter, dplyr, readr, tidyr, purr, tibble, stringr, forcats
 library(ggplot2)
-#library(magritter)
 
 #library(help="datasets") #suggested to find datasets
+read_csv("data/myfile.csv") #need a data folder first for datasets being used
