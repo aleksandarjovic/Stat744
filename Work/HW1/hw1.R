1fd2eda4f72c411345eb6bbcc22e3b279dcdc17b
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-15 21:15:25 -0500

    .

--- Work/HW1/hw1.R
+++ Work/HW1/hw1.R
@@ -1,8 +1,14 @@
-rm(list = ls())### Homework 1. Tues Jan 16. Aleksandar Jovic###
-#library(help="datasets") #suggested to find datasets
-#######Look up violin plots later, boxplots with probability density
+### Homework 1. Tues Jan 16. Aleksandar Jovic ###
+rm(list = ls())
 
+#######Look up violin plots later, boxplots with probability density
 library(tidyverse) #loads magritter, dplyr, readr, tidyr, purr, tibble, stringr, forcats
 library(ggplot2)
+library(GGally)
+#library(pgmm) #contains datasets: {coffee, olive, wine}
+#library(help="datasets") #suggested to find datasets *your data set should have >100 obs and 4 var, at least one categorical, one continuous variable
 
-read_csv("data/myfile.csv") #need a data folder first for datasets being used
+theme_set(theme_bw())
+
+wine.dat=read_csv("Data/Wine.csv") #DIFFERENT wine set from the pgmm one
+names(wine.dat)=c('','','','') #14 columns
