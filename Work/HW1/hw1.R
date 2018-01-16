5e220fd0f79f6437b31a66dede8dc036c97e8a19
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-16 01:06:46 -0500

    for tomorrow q's

--- Work/HW1/hw1.R
+++ Work/HW1/hw1.R
@@ -7,9 +7,11 @@ library(GGally) #trusty sidekick
 
 theme_set(theme_bw()) #override basic ggplot, can edit further later
 
-wined=read_csv('Data/Wine.csv') #credited to: http://archive.ics.uci.edu/ml/datasets/Wine
+wined=read_csv('Data/Wine.csv') #credited to: http://archive.ics.uci.edu/ml/datasets/Wine, column 1 is the class
 names(wined)=c('class','alc','malic','ash','alcalin','mg','phenol','flavan','nonflav','proanth','col.int','hue','diluted','prol')
 
-wined= wined %>% mutate(class=as.factor(class)) #the three wine classes are coded as {1,2,3} numerics originally
+wined= wined %>% mutate(class=as.factor(class)) #wine classes are originally coded as {1,2,3} numerics
 
 #ggpairs(wined, aes(colour=class, alpha=.5)) #EXTREMELY slow with this dataset, but good to check for a quick diagnostic
+#ggsave("Work/HW1/pairs.pdf")
+
