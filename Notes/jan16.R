b7dbb4ea74d122e9e72a0cfdf6a430d1287a9154
Aleksandar Jovic <you@example.com>
2018-01-16 14:34:02 -0500

    midway class

--- Notes/jan16.R
+++ Notes/jan16.R
@@ -1,2 +1,13 @@
-#jan16
-#why is data$variable less preferred over data%>% mutate(class), strictly ease of read? seems to go against programming basics and relearning something (not seeing the front end of what's really happening)
+#jan16 # continuous on y, factor on x, facet it, vs scatterplots, vs .... maybevilin/box for the side see the pairs
+#lecture file is: stat744/lectures/principles_1.rmd
+#information at the time of need refers to use legend when absolutely needed
+#tufte boxplot may be going overload, dont always need so minimalist that it's too unclear
+##ie. use gridlines maybe (feint) if it could help judge where things are
+##dynamic graphics (where you can mouseover and read numbers) can replace gridlines
+
+## DIRECT LABELS DOCUMENTATION FOR LABELLING GRAPHS IN NOTES ###
+
+#figure out what is most imortant so it goes on the x
+
+#Speed up large scatterplots by specifying
+pch="." #using dots rather than circles is way faster
