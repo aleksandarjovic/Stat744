9cf24b1d28d0db1c48d9d4391a99dab50caaf71e
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-15 22:15:54 -0500

    getting there. dataset set up correctly

--- Work/HW1/hw1.R
+++ Work/HW1/hw1.R
@@ -1,14 +1,15 @@
 ### Homework 1. Tues Jan 16. Aleksandar Jovic ###
 rm(list = ls())
 
-#######Look up violin plots later, boxplots with probability density
 library(tidyverse) #loads magritter, dplyr, readr, tidyr, purr, tibble, stringr, forcats
 library(ggplot2)
-library(GGally)
-#library(pgmm) #contains datasets: {coffee, olive, wine}
-#library(help="datasets") #suggested to find datasets *your data set should have >100 obs and 4 var, at least one categorical, one continuous variable
+library(GGally) #trusty sidekick
 
-theme_set(theme_bw())
+theme_set(theme_bw()) #override basic ggplot, can edit further later
 
-wine.dat=read_csv("Data/Wine.csv") #DIFFERENT wine set from the pgmm one
-names(wine.dat)=c('','','','') #14 columns
+wined=read_csv('Data/Wine.csv') #credited to: http://archive.ics.uci.edu/ml/datasets/Wine
+names(wined)=c('class','alc','malic','ash','alcalin','mg','phenol','flavan','nonflav','proanth','col.int','hue','diluted','prol')
+
+wined= wined %>% mutate(class=as.factor(class)) #the three wine classes are coded as {1,2,3} numerics originally
+
+#ggpairs(wined, aes(colour=class, alpha=.5)) #EXTREMELY slow with this dataset, but good to check for a quick diagnostic
