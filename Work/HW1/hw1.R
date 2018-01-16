d4f2731c40df6754981833a2283a6491eff1ffe4
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-16 18:22:15 -0500

    cit+id

--- Work/HW1/hw1.R
+++ Work/HW1/hw1.R
@@ -1,6 +1,8 @@
 ### Homework 1. Tues Jan 16. Aleksandar Jovic ###
 rm(list = ls())
 
+# continuous on y, factor on x, facet it, vs scatterplots, vs .... maybevilin/box for the side see the pairs
+
 library(tidyverse) #loads magritter, dplyr, readr, tidyr, purr, tibble, stringr, forcats
 library(ggplot2)
 library(GGally) #trusty sidekick
@@ -15,3 +17,6 @@ wined= wined %>% mutate(class=as.factor(class)) #wine classes are originally cod
 #ggpairs(wined, aes(colour=class, alpha=.5)) #EXTREMELY slow with this dataset, but good to check for a quick diagnostic
 #ggsave("Work/HW1/pairs.pdf")
 
+#citation()
+#citation('tidyverse')
+#citation('ggplot2')
