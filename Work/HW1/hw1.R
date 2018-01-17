676eb70639fb146e643b55887bb7d45489cf24d2
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-16 21:52:53 -0500

    x

--- Work/HW1/hw1.R
+++ Work/HW1/hw1.R
@@ -1,22 +1,33 @@
 ### Homework 1. Tues Jan 16. Aleksandar Jovic ###
+
+## Headers ##
 rm(list = ls())
-
-# continuous on y, factor on x, facet it, vs scatterplots, vs .... maybevilin/box for the side see the pairs
-
 library(tidyverse) #loads magritter, dplyr, readr, tidyr, purr, tibble, stringr, forcats
 library(ggplot2)
 library(GGally) #trusty sidekick
-
 theme_set(theme_bw()) #override basic ggplot, can edit further later
 
-wined=read_csv('Data/Wine.csv') #credited to: http://archive.ics.uci.edu/ml/datasets/Wine, column 1 is the class
+## Dataset ##
+wined=read_csv('Data/Wine.csv')
+#citation: http://archive.ics.uci.edu/ml/datasets/Wine, column 1 is the class
+#This dataset is a multivariate dataset which could be used for classification analysis.
 names(wined)=c('class','alc','malic','ash','alcalin','mg','phenol','flavan','nonflav','proanth','col.int','hue','diluted','prol')
 
 wined= wined %>% mutate(class=as.factor(class)) #wine classes are originally coded as {1,2,3} numerics
 
-#ggpairs(wined, aes(colour=class, alpha=.5)) #EXTREMELY slow with this dataset, but good to check for a quick diagnostic
-#ggsave("Work/HW1/pairs.pdf")
+#ggpairs(wined, aes(colour=class, alpha=.5)) #EXTREMELY slow with this dataset, but good to check for a quick overview to estimate important variables for discriminating wine types
+#ggsave("Work/HW1/pairs.pdf") #the pairs plot is in the HW1 directory
 
-#citation()
-#citation('tidyverse')
-#citation('ggplot2')
+## Plot 1 ##
+#
+
+plot1=ggplot(
+  data=wined,aes(x=age,y=weigt)
+)
+
+# continuous on y, factor on x, facet it, vs scatterplots, vs .... maybevilin/box for the side see the pairs
+#maybe do another dataset showing time series plot, and time series scatterplot (need multidimensional perhaps predator prey)
+## Citations ##
+citation()
+citation('tidyverse')
+citation('ggplot2')
