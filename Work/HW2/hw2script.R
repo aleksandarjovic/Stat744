f47833555f9a4fff60e87ebff0b7e07906125f5d
Aleksandar Jovic <jovica@mcmaster.ca>
2018-01-23 01:21:21 -0500

    rough

--- Work/HW2/hw2script.R
+++ Work/HW2/hw2script.R
@@ -1,1 +1,34 @@
 ### HW2 ###
+
+#remove this: use true, do not clear memory for user#
+library(tidyverse) #loads magrittr, dplyr, readr, tidyr, purr, tibble, stringr, forcats, ggplot2
+library(GGally)
+
+x=readr::read_csv("https://bbolker.github.io/stat744/data/vaccine_data_online.csv")
+
+#Reproducing original graph
+plot1=(ggplot(data=x,aes(x=disease,y=year,alpha=0.5))+
+         geom_point()+
+         #scale_size_area(size=cases)+
+         labs(title="Recreation of Jia You's Vaccine",x="Disease", y="Year")
+)
+print(plot1)
+#ggsave("Work/HW2/Plot1.pdf")
+# Using AREA rather than RADIUS (since radius grows by a squared factor -- misleading)
+
+
+##Time series (simple but most natural, no reason to overcomplicate)
+
+##Time series with vaccine shown as center
+
+##How effective are vaccines? graphing rate of change (cut off until start of vaccine?) faster decrease is better.
+
+
+#extra credit hypothetical... i.e. creation of chart, and how to use data
+
+
+## Citations ##
+citation()
+citation('tidyverse') #et al.
+citation('GGally')
+#You, Jia. "Here’s the visual proof of why vaccines do more good than harm". www.sciencemag.org. April 27, 2017.
