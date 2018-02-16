326a58612894c6f97c8a0429157b5254369a0115
Aleksandar Jovic <jovica@mcmaster.ca>
2018-02-15 22:27:06 -0500

    hm3 rough finish. (essential code done)

--- Work/HW3/hw3script.R
+++ Work/HW3/hw3script.R
@@ -1,4 +1,4 @@
-### Homework 2: Why do we let doctors give our kids vaccines? Aleksandar Jovic. Jan 24, 2018 ### I'm still working on R markdowns, so I'll put all comments in here for now.
+### Homework 4 (3*): Finding a way to make a chart into a useful graph.  Aleksandar Jovic. Feb 15, 2018 ###
 
 
 #~Preamble~#    (taken complete from note)
@@ -21,25 +21,117 @@ library(readr)
 
 #~Dataset~#
 x1=read_csv('Data/POCIS_Raw_McCallum.csv') #McCallum et al (2017). Full citation below
-#we don't need the ID nor the sampler type (All are polar organic chemical integrative samplers)
-x1=x1[,-c(1,2)]
+x1=x1[,-c(1,2)] #we don't need the ID nor the sampler type (All are polar organic chemical integrative samplers)
 x2=read_csv('Data/POCIS_support.csv') #to get some more informative column names
 
-#Firstly, this graph
+#
 
-x=(x1 %>%
-     group_by(Site) %>%
-     summarize(
-       
-     )
-   )  
-
-g1=(ggplot(data=x1)+
-      geom_boxplot(aes(x=Site,y=CFN))
+vert=(
+  x1 %>%
+    gather(drug,conc,CFN:PPN) %>%
+    full_join(x2,by=c("drug"="abbr"))
 )
-print(g1)
-#ggsave("Work/HW3/asljfl.pdf") 
+#some pesky NA rows existing for sites, were these drugs never tested for? They have no concentration measurements.
+vert=vert%>%filter(!is.na(Site))  #this takes care of a lot of problems (removing empty columns, and multiple empty charts)
+
+#This first graph compares all
+g1=(ggplot(data=vert)+
+      geom_boxplot(aes(x=Site,y=conc))+
+      facet_wrap(~drug)
+)
+print(g1); #ggsave("Work/HW3/asljfl.pdf") 
+
+#boxplot not technically appropriate with such low, data points (triplicate), even though it helps visualize the general area
+g1a=(ggplot(data=vert)+
+      geom_dotplot(binaxis = "y", stackdir ="center", aes(x=Site,y=conc))+
+      facet_wrap(~drug)
+)
+print(g1a); #ggsave("Work/HW3/asljfl.pdf") 
+
+#However, with only 3 points, the dots themselves don't really show a good spread when the scales are not appropriate (same with the boxplots)
+#Sucralose (SUC) skews the rest of the data (since we want to keep it on a common y axis if we want to compare magnitudes)
+
+vert_noSUC=vert %>% filter(drug!='SUC')
+
+g2=(ggplot(data=vert_noSUC)+
+      geom_boxplot(aes(x=Site,y=conc))+
+      facet_wrap(~drug)
+)
+print(g2); #ggsave("Work/HW3/asljfl.pdf") 
+
+#boxplot not technically appropriate with such low, data points (triplicate), even though it helps visualize the general area
+g2a=(ggplot(data=vert_noSUC)+
+       geom_dotplot(binaxis = "y", stackdir ="center", aes(x=Site,y=conc))+
+       facet_wrap(~drug)
+)
+print(g2a); #ggsave("Work/HW3/asljfl.pdf") 
+
+#Note, this is a temporary remedy. Ideally, I could just show 24 different graphs, one for each drug. This way, there could be an adequate comparison for each specific drug.
+#I originally just had them faceted in case someone is interested in comparisons... however, since in biology, different drugs have different effects at various concentrations... i.e. High levels of sucralose, and high levels of testosterone need to be kept in context (each one have "high" defined differently).
+
+#For example, here is testosterone on its own:
+tes=vert%>%filter(drug=="TST")
+g3=(ggplot(data=tes)+
+      geom_boxplot(aes(x=Site,y=conc))
+)
+print(g3); #ggsave("Work/HW3/asljfl.pdf") #Won't be printing this one since it's a flatline at 0... however, it is interesting to note that the dotplot is quite a bad choice for this one as it looks like there are 3 different values.
+#oops, bad example, didn't realize all values were 0 on the chart, I'll leave this in.
+#let's instead try Naproxen
+npx=vert%>%filter(drug=="NPX")
+g3a=(ggplot(data=npx)+
+      geom_boxplot(aes(x=Site,y=conc))
+)
+print(g3a); #ggsave("Work/HW3/asljfl.pdf")
+#Much more informative. This way, each axis would be scaled respectively. 
+
+#?????????  General Question  ?????????????????????#
+#I considered standardizing all of the values, and plotting them faceted, to compare them, but I don't know an easy way for python to go through each class of drug, calculate its respective mean/sd then standardize.
+#Even if I were to do it in some for loop, it seems like a task that would need to be done manually in R --> meaning, going through each section individually.
+#Languages like python seem better at distinguishing between different strings of text, using an algorithm where I store the string of characters to a temp slot, then if the next row is the same class, to incorporate that value:
+#When a new class is reached, the stored data would then be used to standardize the data into a new column, and the class would be changed to the new string. This would then repeat until the end of the dataset.
+#Unless I'm mistaken, this is one of R's limitations as a language.
+###################################################
+
+
+#dmSRT and dmVLF are variations of their respective parents, and are also anti-depressants, as such, I'll impute that data into the tibble.
+imput=vert
+imput$drugcat[which(is.na(imput$drugcat))] <-'antidepressant'   #how would you tidyverse this nicely... the traditional method beckoned and I couldn't resist its call.
+
+#now we can see how much each body of water is poluted with each respective drug category
+g4=(ggplot(data=imput)+
+      geom_boxplot(aes(x=Site,y=conc))+
+      facet_wrap(~drugcat)
+)
+print(g4); #ggsave("Work/HW3/asljfl.pdf")
+
+#similar to sucralose dominating the data when drug was in question, now food is dominating, so let's take it out.
+
+nofood=imput%>%filter(drugcat!='food')
+g4a=(ggplot(data=nofood)+
+      geom_boxplot(aes(x=Site,y=conc))+
+      facet_wrap(~drugcat)
+)
+print(g4a); #ggsave("Work/HW3/asljfl.pdf")
+
+#Once again, we could just do this individually for each graph for the best look if we're more interested in a detailed comparison within specific drug classes instead of a general comparison
+#Quick note, boxplots are better to use here, since when we group by drug category, rather than individual drug, we can combine more measurements for a more appropriate box plot (If we think it's ok to lump all "beta-blocker" (etc) together)
+#Let's have a look at beta-blockers with a dotplot:
+
+beta=imput%>%filter(drugcat=='beta-blocker')
+g4beta=(ggplot(data=beta)+
+       geom_boxplot(aes(x=Site,y=conc))
+)
+print(g4beta); #ggsave("Work/HW3/asljfl.pdf")
+#dotplot comparison
+g4beta1=(ggplot(data=beta)+
+          geom_dotplot(stackdir="center", binaxis = 'y',aes(x=Site,y=conc))
+)
+print(g4beta1); #ggsave("Work/HW3/asljfl.pdf")
+#Perhaps a bit better representation, but in my opinion, not as "nice" as a boxplot.
 
 
 #~Citations~#
+citation()
+citation('tidyverse')
+citation('ggplot2')
 #McCallum, Erin S., Sherry N. N. Du, Maryam Vaseghi-Shanjani, Jasmine A. Choi, Theresa R. Warriner, Tamanna Sultana, Graham R. Scott, and Sigal Balshine. 2017. “In Situ Exposure to Wastewater Effluent Reduces Survival but Has Little Effect on the Behaviour or Physiology of an Invasive Great Lakes Fish.” Aquatic Toxicology 184 (March): 37–48. doi:10.1016/j.aquatox.2016.12.017
